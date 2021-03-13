5dd1c08386e8122ae30daf956fc99b3d1819dcbd
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-03-13 23:41:47 +0100

    frontend

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{minHeight:"1100px"}}>
             Home
         </div>
     );
---
+++ client/src/Store/reducer/userReducer.js
@@ -0,0 +1,8 @@
+const userReducer = (state = {}, actions) => {
+  switch (actions.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
