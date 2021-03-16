209254a82c06ab819416e1e00dda5166c69c2c34
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-03-16 01:03:12 +0100

    frontend

--- client/src/Components/misc/Form/utils.js
+++ client/src/Components/misc/Form/utils.js
@@ -1,32 +1,45 @@
-
-const validate = (newElement,formdata)=>{
-  let error = [true,""]
-  if (newElement.validation.email){
-      const valid =/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(newElement.value)
-      const message = valid ? "":"Must be an email"
-      error = valid ? error : [valid,message] 
+const validate = (newElement, formdata) => {
+  let error = [true, ""];
+  if (newElement.validation.email) {
+    const valid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(newElement.value);
+    const message = valid ? "" : "Must be an email";
+    error = valid ? error : [valid, message];
   }
-  if (newElement.validation.required){
-      const valid = newElement.value.trim() !== ""
-      const message = valid ? "":"This field is required"
-      error = valid ? error : [valid,message] 
+  if (newElement.validation.required) {
+    const valid = newElement.value.trim() !== "";
+    const message = valid ? "" : "This field is required";
+    error = valid ? error : [valid, message];
   }
-  return error
-}
+  return error;
+};
 
+export const update = (element, formdata) => {
+  let newFormdata = { ...formdata };
+  let newElement = { ...newFormdata[element.id] };
+  newElement.value = element.e.target.value;
 
-export const update = (element,formdata)=>{
+  if (element.blur) {
+    let validata = validate(newElement, formdata);
+    newElement.valid = validata[0];
+    newElement.validationMessage = validata[1];
+  }
+  newElement.touched = element.blur;
+  newFormdata[element.id] = newElement;
+  return newFormdata;
+};
+export const generate = (formdata) => {
+  let datatoSubmit = {};
+  for (let key in formdata) {
+    datatoSubmit[key] = formdata[key].value;
+  }
+  return datatoSubmit;
+};
+export const isFormValid = (formdata)=>{
 
-    let newFormdata = {...formdata}
-    let newElement= {...newFormdata[element.id]}
-    newElement.value = element.e.target.value
+    let formvalid = true
 
-   if(element.blur){
-       let validata = validate(newElement,formdata)
-       newElement.valid = validata[0]
-       newElement.validationMessage = validata[1]
-   }
-    newElement.touched = element.blur
-    newFormdata[element.id] =newElement 
-    return newFormdata
+    for(let key in formdata){
+        formvalid = formdata[key].valid  && formvalid
+    }
+    return formvalid
 }
