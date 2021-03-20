961b80ebfad4828986bb6a441ac1f85971053752
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-03-20 14:58:41 +0100

    register inteerface

--- client/src/Components/RegisterLogin/Login.js
+++ client/src/Components/RegisterLogin/Login.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import FormField from '../misc/Form/formField'
-import {update,generate,isFormValid} from '../misc/Form/utils'
-import {connect} from 'react-redux'
-import {loginUser} from '../../Store/actions/user/user_action'
-import {Link} from 'react-router-dom'
-
+import FormField from "../misc/Form/formField";
+import { update, generate, isFormValid } from "../misc/Form/utils";
+import { connect } from "react-redux";
+import { loginUser } from "../../Store/actions/user/user_action";
+import { Link, withRouter } from "react-router-dom";
 
 class Login extends Component {
   state = {
@@ -24,7 +23,7 @@ class Login extends Component {
           email: true,
         },
         valid: false,
-        touched:false,
+        touched: false,
         validationMessage: "",
       },
       password: {
@@ -39,79 +38,79 @@ class Login extends Component {
           required: true,
         },
         valid: false,
-        touched:false,
+        touched: false,
         validationMessage: "",
       },
     },
   };
 
+  updateform = (element) => {
+    const newFormdata = update(element, this.state.formData, "login");
+    this.setState({
+      formError: false,
+      formData: newFormdata,
+    });
+  };
 
-
-
-
-
-
-updateform = (element)=>{
-   const newFormdata = update(element,this.state.formData,"login")
-   this.setState({
-       formError:false,
-       formData:newFormdata
-   })
-}
-
-
-submitForm = (e)=>{
-    e.preventDefault()
-    let data = generate(this.state.formData)
-    let formValid = isFormValid(this.state.formData)
-    if(formValid){
-   this.props.dispatch(loginUser(data))
-    }else{
-        this.setState({
-            formError:true
-        })
+  submitForm = (e) => {
+    e.preventDefault();
+    let data = generate(this.state.formData);
+    let formValid = isFormValid(this.state.formData);
+    if (formValid) {
+      this.props.dispatch(loginUser(data)).then((response) => {
+        if(response.payload.success){
+          this.props.history.push("/");
+        }else{
+          this.setState({
+            formError: true,
+          });
+        }
+       
+      });
+    } else {
+      this.setState({
+        formError: true,
+      });
     }
-}
-
-
+  };
 
   render() {
     return (
-        <div className="login_wrapper">
-          <div className="login_container">
-            <h2>Login Your Account</h2>
-            <form className="login_form" onSubmit={(e)=>this.submitForm(e)}>
-              <FormField
-              formdata = {this.state.formData.email}
-               change = {(element)=>this.updateform(element)}
-               id="email"
-              />
-              <FormField
-              formdata = {this.state.formData.password}
-               change = {(element)=>this.updateform(element)}
-               id="password"
-              />
-              <input type="submit" name="submit" onClick={(e)=>this.submitForm(e)} />
-              {
-                  this.state.formError? (
-                      <div className="error_label">
-                          Invalid username or Password
-                      </div>
-                  ):null
-              }
-            </form>
-            <div>
-              <span>
-                <a href="/">Forgot Password?</a>
-              </span>
-              <Link to="/register">
-                <a href="/">Create An Account</a>
-              </Link>
-            </div>
+      <div className="login_wrapper">
+        <div className="login_container">
+          <h2>Login Your Account</h2>
+          <form className="login_form" onSubmit={(e) => this.submitForm(e)}>
+            <FormField
+              formdata={this.state.formData.email}
+              change={(element) => this.updateform(element)}
+              id="email"
+            />
+            <FormField
+              formdata={this.state.formData.password}
+              change={(element) => this.updateform(element)}
+              id="password"
+            />
+            <input
+              type="submit"
+              name="submit"
+              onClick={(e) => this.submitForm(e)}
+            />
+            {this.state.formError ? (
+              <div className="error_label">Invalid username or Password</div>
+            ) : null}
+          </form>
+          <div>
+            <span>
+              <a href="/">Forgot Password?</a>
+            </span>
+            <Link to="/register">
+              <a href="/">Create An Account</a>
+            </Link>
           </div>
         </div>
+      </div>
     );
   }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
